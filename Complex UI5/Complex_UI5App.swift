5313a2ab369c22380ca0d03d253cc5feb82858fb
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-10 23:03:27 +0530

    Initial Commit

---
+++ Complex UI5/Complex_UI5App.swift
@@ -0,0 +1,17 @@
+//
+//  Complex_UI5App.swift
+//  Complex UI5
+//
+//  Created by Mahesh Prasad on 10/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Complex_UI5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
